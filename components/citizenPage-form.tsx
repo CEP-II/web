f5a7267a73a4fc807fe03aa60eab4cf2ae9e6e62
Msitua <ask@boegedal.com>
2023-05-18 15:45:54 +0200

    fixed topbar auto height

--- components/citizenPage-form.tsx
+++ components/citizenPage-form.tsx
@@ -110,18 +110,23 @@ const MyComponent = () => {
     <div>
  
 
- <div className={styles.color_topbar} style={{ position: 'absolute', top: '0%', left: '0%', width: '100%', height: '12%' }}>
+ <div className={styles.color_topbar} style={{ position: 'absolute', top: '0%', left: '0%', width: '100%', height: 'auto' }}>
   <h1 style={{ textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: '3vw' }}>Night Assist</h1>
   <h3 style={{ textAlign: 'center', color: 'white', fontSize: '2vw' }}>Your Guide through the night</h3>
 
   {/* button to log out */}
-  <button className="btn btn-primary" style={{ position: 'absolute', top: '30%', right: '3%', height: '40%', width: '10%', fontSize: '1vw' }} onClick={() => {
+  <button className="btn btn-primary" style={{ position: 'absolute',  top: '50%',transform: 'translateY(-50%)', right: '3%', fontSize: '1vw' }} onClick={() => {
     Cookies.remove('token');
     Cookies.remove('citizenId');
     window.location.href = '/';
   }}>Log out</button>
 
-<select className="btn"id="limitDropdown" style={{position: 'absolute', top:'30%', height: '40%', width:'14%', fontSize:'2vw', color:'white'}} value={limit} onChange={handleLimitChange}>
+<div style={{position: 'absolute', top: '50%',transform: 'translateY(-50%)', left: '10px'}}>
+
+
+
+
+<select className="btn"id="limitDropdown" style={{fontSize:'1.5vw', color:'white'}} value={limit} onChange={handleLimitChange}>
 
 <option value="10">10</option>
 <option value="15">15</option>
@@ -129,6 +134,7 @@ const MyComponent = () => {
 <option value="30">30</option>
 <option value="50">50</option>
 </select>
+</div>
 
 
 </div>
@@ -136,17 +142,19 @@ const MyComponent = () => {
 
      
             
-          <div className="flex flex-col" style={{ position: 'absolute', top: '14%', left: '50%', transform: 'translateX(-50%)', width: '100%' , height: '100%'}}>
+<div className="flex flex-col" style={{ position: 'absolute', top: '14%', left: '50%', transform: 'translateX(-50%)', width: '100%' , height: '100%'}}>
             {/* Render list items */}
             <div className="bg-gray.200">
             <h1 style={{textAlign: 'center', color: 'black', fontWeight: 'bold', fontSize: '2vw'}}>Your Timestamps</h1>
             <div
   style={{
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(290px, 1fr))',
-    gap: '1%',
-    margin: '10px auto',
+    gridTemplateColumns: `repeat(auto-fit, minmax(${25}ch, 1fr))`,
+    gap: '10px',
+    margin: '2%',
     maxWidth: '95%',
+    overflow: 'auto',
+    fontSize: '1.2rem',
   }}
 >
   {/* Render list items */}
@@ -168,20 +176,20 @@ const MyComponent = () => {
           boxShadow: '0 0 10px rgba(0, 0, 0, 0.15)',
           cursor: 'pointer',
           background: selectedItemIndex === index ? '#dae1e3' : '#fff',
-          width: '95%',
-          maxWidth: '500px',
+          
           minWidth: 'fit-content', // Set the minWidth property to fit the content width
-          margin: '1%',
         }}
         onClick={() => {
           handleItemClick(index, item);
         }}
       >
+        <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Citizen: {typeof item.citizen === 'object' ? item.citizen.name : findCitizen(item.citizen)}</p>
         <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Start time: {startTime.toLocaleString()}</p>
-        <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Position: {item.position}</p>
+        <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Position: {item.positionId}</p>
         <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Duration: {duration} ms</p>
         <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>Device ID: {item.deviceId}</p>
         <p style={{ fontSize: '1.2rem', margin: '0.5rem 0', whiteSpace: 'nowrap' }}>ID: {item._id}</p>
+
       </div>
     );
   })}
@@ -212,42 +220,44 @@ const MyComponent = () => {
             <div
               style={{
                 display: 'grid',
-                gridTemplateColumns: 'repeat(auto-fit, minmax(290px, 1fr))',
+                gridTemplateColumns: `repeat(auto-fit, minmax(${25}ch, 1fr))`,
                 gap: '10px',
-                margin: '10px auto',
+                margin: '2%',
                 maxWidth: '95%',
+                overflow: 'auto',
+                fontSize: '1.2rem',
               }}
             >
               {/* Render list items */}
-              {accidents.map((item, index) => {
-           const alarmTime = new Date(item.alarmTime);
-
-          return (
-            <div key={index}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              padding: '2%',
-              
-              borderRadius: '10px',
-              boxShadow: '0 0 10px rgba(0, 0, 0, 0.15)',
-              cursor: 'pointer',
-              background: selectedItemIndex === index ? '#dae1e3' : '#fff',
-              
+              {timeStamps.map((item, index) => {
+                 const alarmTime = new Date(item.alarmTime);
+      
+                return (
+                  <div key={index}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '2%',
+                    borderRadius: '10px',
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.15)',
+                    cursor: 'pointer',
+                    background: selectedItemIndex === index ? '#dae1e3' : '#fff',
+                    
+                    minWidth: 'fit-content', // Set the minWidth property to fit the content width
+                  
+                  }}
+                    onClick={() => {handleItemClick(index, item)}}
+                   >
+                  <p style={{ fontSize: '1.2rem', margin: '0.2rem 0', whiteSpace: 'nowrap'}}>Citizen: {typeof item.citizen === 'object' ? item.citizen.name : findCitizen(item.citizen)}</p>
+                  <p style={{fontSize: '1,2rem', margin: '0.2rem 0', whiteSpace: 'nowrap'}}>Time of Accident: {alarmTime.toLocaleString()}</p>
+                  <p style={{fontSize: '1.2rem', margin: '0.2rem 0', whiteSpace: 'nowrap'}}>Position: {item.positionId}</p>
+                  <p style={{fontSize: '1.2rem', margin: '0.2rem 0', whiteSpace: 'nowrap'}}>Device ID: {item.deviceId}</p>
             
-            }}
-              onClick={() => {handleItemClick(index, item)}}
-            
-            >
-              <p style={{ fontSize: '14px', margin: '0px' }}>Time of Accidents: {alarmTime.toLocaleString()}</p>
-              <p style={{ fontSize: '14px', margin: '0px' }}>Possition: {item.positionId.toLocaleString()}</p>
-              <p style={{ fontSize: '14px', margin: '0px' }}>Device ID: {item.deviceId}</p>
-              
-            </div>
-          );
-        })}
+                  </div>
+                );
+              })}
             </div>
       
       
